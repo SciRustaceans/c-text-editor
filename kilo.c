21e2707480569e3659a4406908b538b82c8ed057
SciRustaceans <jaldabe@proton.me>
2025-09-10 20:07:03 +0200

    added append buferr logic to code

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
@@ -70,11 +71,11 @@ int getCursorPosition(int *rows, int *cols) {
         i++;
     }
     buf[i] = '\0';
+    
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
 
-    printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
-
-    editorReadKey();
-    return -1;
+    return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
@@ -89,22 +90,53 @@ int getWindowSize(int *rows, int *cols) {
         return 0;
     }
 }
+/*** Append Buffer ***/ 
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend( struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
 /*** OUTPUT ***/ 
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        abAppend(ab, "~", 1);
+        
+        if (y < E.screenrows - 1) {
+            abAppend(ab, "\r\n", 2);
+        }
     }
 }
 
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
+    
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H" , 3);
 
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[H", 3);
-}
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+} 
 /*** Input ***/
 
 void editorProcessKeypress() {
